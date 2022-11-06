0c9ac99cdb59d57d1d1b0bb56552aa3ba6b6f9cb
bl4szk4 <blachabartlomiej@gmail.com>
2022-11-06 17:45:30 +0100

    a

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir LOGS
-uvicorn --host 192.168.56.1 --port 8888 --workers 1 --log-config "./logging.conf" --log-level "info" --use-colors main:app
+uvicorn --host 192.168.0.67 --port 8888 --workers 1 --log-config "./logging.conf" --log-level "info" --use-colors main:app
