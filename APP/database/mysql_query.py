0347774e5d18734336c503da2f47fb3b23e4ccd2
pkarmanski <pkarmanski@edz.com.pl>
2022-11-20 16:00:24 +0100

    get post return login and upload_date of the post

--- APP/database/mysql_query.py
+++ APP/database/mysql_query.py
@@ -7,7 +7,7 @@ class MysqlQuery(Enum):
     REGISTER_USER = 'insert into users (login, password, email, faculty_id, status) values("{}",MD5("{}"), "{}", {}, "{}")'
     LOGIN_USER = 'select id from users where login="{}" and password=MD5("{}") and status = "{}"'
     GET_USER_ID = 'select id from users where login="{}" and password=MD5("{}") and email="{}"'
-    GET_POSTS = 'select id, content, image from post where lower(status)="{}" order by upload_date limit {}'
+    GET_POSTS = 'select post.id, post.content, post.image, post.upload_date as uploadDate, users.login from post join users on users.id = post.user_id where lower(post.status)="{}" order by post.upload_date desc limit {}'
     GET_FACULTY_LIST = 'select id, name from faculty'
     GET_FILED_OF_STUDY_LIST = 'select id, name from field_of_study'
     GET_COURSE_LIST = 'select id, name from course'
--- APP/routers/studentrade.py
+++ APP/routers/studentrade.py
@@ -24,10 +24,10 @@ def login_user(login_user_data: LoginUser):
     return service.login_user(login_user_data)
 
 
-@router.get('/getPosts/{userName}')
-def get_post(userName: str):
+@router.get('/getPosts')
+def get_post():
     log_id = str(int(time.time()))
-    service = Service(log_id, userName)
+    service = Service(log_id, '')
     return service.get_posts()
 
 
